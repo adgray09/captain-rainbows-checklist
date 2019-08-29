506c66d5ffb85a9c954ce09a7a7c937a0c514c86
ALEX_GRAY <alexandergray37@yahoo.com>
2019-08-28 21:08:16 -0700

    added CRUD functions

---
+++ checklist.py
@@ -0,0 +1,38 @@
+print("Hello World")
+
+checklist = list()
+checklist.append('Blue')
+print(checklist)
+checklist.append('Orange')
+print(checklist)
+
+def create(item):
+    checklist.append(item)
+
+def read(index):
+    item = checklist[index]
+    return item
+checklist = ['Blue', 'Orange']
+checklist[1] = 'Cats'
+print(checklist)
+
+def update(index, item):
+    checklist[index] = item
+
+def destroy(index):
+    checklist.pop(index)
+
+def test():
+    create("purple sox")
+    create("red cloak")
+
+    print(read(0))
+    print(read(1))
+
+    update(0, "purple socks")
+    destroy(1)
+
+    print(read(0))
+    print(read(1))
+
+    test()
