e230b664486a7d4ca30ba34ae32a9079471554a2
ALEX_GRAY <alexandergray37@yahoo.com>
2019-09-02 22:38:57 -0700

    added final functionality

--- checklist.py
+++ checklist.py
@@ -1,24 +1,18 @@
-#print("Hello World")
-checklist = []
-#checklist.append('Blue')
-#print(checklist)
-#checklist.append('Orange')
-#print(checklist)
+checklist = list ()
 
-
+#Create
 def create(item):
-    #checklist.append(item)
+    checklist.append(item)
 
+#Read
 def read(index):
     return checklist[index]
-    #return item
-#checklist = ['Blue', 'Orange']
-#checklist[1] = 'Cats'
-#print(checklist)
 
+#Update
 def update(index, item):
     checklist[index] = item
 
+#Destroy
 def destroy(index):
     checklist.pop(index)
 
@@ -27,11 +21,12 @@ def destroy(index):
 def list_all_items():
     index = 0 
     for list_item in checklist:
-        print(str(index) + list_item)
+        print("{} {}".format(index, list_item))
         index += 1 
 
-def marked_complete(index):
-    checklist(index, "√" + checklist[index])
+def marked_completed():
+    for list_item in checklist:
+        print ("√ {}".format(list_item))
 
 def select(function_code):
     if function_code == "C":
@@ -49,6 +44,15 @@ def select(function_code):
     elif function_code == "Q":
         return False   
 
+    elif function_code == "U":
+        update_index = user_input("Index number?")
+        update_name = user_input("New name")
+        update(int(update_index), update_name)
+
+    elif function_code == "D":
+        index = int(user_input("index:"))
+        destroy(index)
+
     else:
         print("Unknown Option")
     return True
@@ -57,19 +61,22 @@ def user_input(prompt):
     user_input = input(prompt)
     return user_input
 
-def test():
-    list_all_items()
-    create("purple sox")
-    create("red cloak")
-    print(read(0))
-    print(read(1))
+#Test
+#def test():
+    #list_all_items()
 
-    update(0, "purple socks")
-    destroy(1)
-    
-    print(read(0))
+   # create("purple sox")
+   #create("red cloak")
+
+   # print(read(0))
     #print(read(1))
-    list_all_items()
+
+   # update(0, "purple socks")
+    #destroy(1)
+    
+    #print(read(0))
+    #print(read(1))
+    #list_all_items()
     #select("C")
     #list_all_items()
     #select("R")
@@ -78,13 +85,14 @@ def test():
     #user_value = user_input("Please Enter a Value:")
     #print(user_value)
 
-test()
+#test()
 
 running = True
 while running:
     selection = user_input(
-        "Press C to add to list, R to Read from list, P to display list, and Q to quit")
+        "Press C to add to list, U to update, R to Read from list, P to display list, D to destroy, and Q to quit")
     running = select(selection)
+    marked_completed()
 
 
 
